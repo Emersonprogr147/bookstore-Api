b622a13313e7dd269bd4b9a594426a0d67816a81
Emersonprogr147 <ermesantos573@gmail.com>
2023-01-06 13:54:05 -0300

    repository com falha ao executar banco h2

--- src/main/java/com/emerson/bookstore/repositories/CategoriaRepository.java
+++ src/main/java/com/emerson/bookstore/repositories/CategoriaRepository.java
@@ -4,9 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.emerson.bookstore.domain.Categoria;
-
 @Repository
-
 public interface CategoriaRepository extends JpaRepository< Categoria , Integer > {
 
 }
--- src/main/java/com/emerson/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/emerson/bookstore/repositories/LivroRepository.java
@@ -4,9 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.emerson.bookstore.domain.Livro;
-
 @Repository
-
 public interface LivroRepository extends JpaRepository< Livro , Integer > {
 
 }
